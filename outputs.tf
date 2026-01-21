0b8b741ba839a648adb83a18b7ee34a2a9365d79
suraj vishwakarma <dev.surajv@gmail.com>
2026-01-21 15:40:39 +0000

    Uncomment EC2 instance output configurations in Terraform files

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,6 @@
-/* output "instance_id" {
+/*
+
+output "instance_id" {
   description = "ID of the EC2 instance"
   value       = aws_instance.app_server.id
 }
@@ -6,4 +8,10 @@
 output "instance_ip" {
   description = "Private IP address of the EC2 instance"
   value       = aws_instance.app_server.private_ip
-} */
+} 
+
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.app_server.public_ip
+}
+*/  
