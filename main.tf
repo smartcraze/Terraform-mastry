7666c60ce098e88405447f1da0c67ee08b35645c
suraj vishwakarma <dev.surajv@gmail.com>
2026-01-21 14:50:06 +0000

    Refactor Terraform configuration: separate provider, data, output, and variable definitions into distinct files

--- main.tf
+++ main.tf
@@ -1,17 +1,3 @@
-provider "aws" {
-  region = "ap-south-1"
-}
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
 
 resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
@@ -21,3 +7,5 @@ resource "aws_instance" "app_server" {
     Name = "learn-terraform"
   }
 }
+
+
