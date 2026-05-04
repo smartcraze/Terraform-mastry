4498f41c28a79da384f68289efd318f26e1f590d
Suraj Vishwakarma <dev.surajv@gmail.com>
2026-05-04 13:43:22 +0000

    chore: add variables for EC2 instance configuration

--- basic/variable.tf
+++ basic/variable.tf
@@ -4,3 +4,23 @@ variable "aws_region" {
   default     = "ap-south-1"
 }
 
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
+variable "aws_ami_id" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+}
+
+variable "instance_root_size" {
+  description = "Root volume size in GB"
+  type        = number
+}
+
+variable "ec2_name" {
+  description = "Name tag for the EC2 instance"
+  type        = string
+}
+
